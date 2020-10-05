ab0f6c8b734dca8492df6d996b05d9fee807ece4
rachel-ng <rng@stuy.edu>
2020-10-04 23:47:14 -0400

    tests

--- 01_squareMatrix/tests.cpp
+++ 01_squareMatrix/tests.cpp
@@ -9,19 +9,55 @@ SquareMatrix<int> s(10);
 SquareMatrix<int> m(10);
 SquareMatrix<int> a;
 
-TEST_CASE("resize"){
-    CHECK(s.size() == 10);
-    CHECK(m.size() == 10);
-    CHECK(a.size() == 0);
+TEST_CASE("size"){
+    std::cout << "\ntesting size\n" << std::endl;
 
+    CHECK(s.size() == 10);
     s.print();
+
+    CHECK(m.size() == 10);
     m.print();
+    
+    CHECK(a.size() == 0);
+    a.print();
+
+    a.resize(1);
+    CHECK(a.size() == 1);
     a.print();
 }
 
 TEST_CASE("== / !="){
+    std::cout << "\ntesting == and !=\n" << std::endl;
+
     bool c = s == m;
     CHECK(c);
+
+    c = s != m;
+    CHECK(!c);
+
+    c = a == m;
+    CHECK(!c);
+
+    c = a != s;
+    CHECK(c);
+
+    for (int r = 0; r < 10; r++) {
+        for (int c = 0; c < 10; c++) {
+            s.at(r,c) = r*10 + c;
+        }
+    }
+    
+    c = s == m;
+    CHECK(!c);
+
+    c = s != m;
+    CHECK(c);
+
+    s.resize(10);
+
+    c = s == m;
+    CHECK(c);
+
     c = s != m;
     CHECK(!c);
 }
@@ -29,28 +65,69 @@ TEST_CASE("== / !="){
 TEST_CASE("resize"){
     std::cout << "\ntesting resize\n" << std::endl;
 
-    s.at(0,0) = 666;
+    std::cout << "s.resize(5)" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        s.at(i,i) = 666;
+    }
+    s.print();
 
     s.resize(5);
-
-    CHECK(s.at(0,0) != 666);
-
     CHECK(s.size() == 5);
-    bool c = s == m;
-    CHECK(!c);
+    s.print();
+
+    // verify resize destroys previous contents
+    for (int i = 0; i < 5; i++) {
+        CHECK(s.at(i,i) != 666);
+    }
+
+    bool errCaught = false;
+    for (int i = 5; i < 10; i++) {
+        errCaught = false;
+        try {
+            s.at(i,i);
+        } catch (const std::out_of_range& e) {
+            errCaught = true;
+        }
+        CHECK(errCaught);
+    }
+
+    bool c = s != m;
+    CHECK(c);
+
+    std::cout << "m.resize(6), m.resize(5)\n" << std::endl;
+    m.resize(6);
+    CHECK(m.size() == 6);
     c = s != m;
     CHECK(c);
 
     m.resize(5);
+    CHECK(m.size() == 5);
     c = s == m;
     CHECK(c);
-    c = s != m;
-    CHECK(!c);
+
+    // verify resize(0) works
+    std::cout << "a.resize(0)" << std::endl;
+    a.print();
+    a.resize(0);
+    CHECK(a.size() == 0);
+    a.print();
 }
 
 TEST_CASE("at(r,c)"){
     std::cout << "\ntesting at(r,c)\n" << std::endl;
 
+    // verify can retrieve
+    std::cout << "populate s" << std::endl;
+    for (int r = 0; r < 5; r++) {
+        for (int c = 0; c < 5; c++) {
+            std::cout << s.at(r,c) << (c < 4 ? ", " : "");
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+
+    // verify can modify
     for (int r = 0; r < 5; r++) {
         for (int c = 0; c < 5; c++) {
             s.at(r,c) = r*10 + c;
@@ -59,43 +136,32 @@ TEST_CASE("at(r,c)"){
 
     s.print();
     m.print();
-    a.print();
 
-    bool c = s == m;
-    CHECK(!c);
+    bool c = s != m;
+    CHECK(c);
 
+
+    // throw exception if rol or column doesn't exist
+    int vals [6][2] = {{6,1},{3,5},{7,7},
+                       {-1,-1},{0,-10},{-8,2}};
     bool errCaught = false;
-    try {
-        s.at(6,1) = 100;
-    } catch (const std::out_of_range& e) {
-        std::cout << "at(6,1) out of range" << std::endl;
-        errCaught = true;
+    for (int i = 0; i < 6; i++) {
+        errCaught = false;
+        try {
+            s.at(vals[i][0],vals[i][1]) = 666;
+        } catch (const std::out_of_range& e) {
+            std::cout << "at(" << vals[i][0] << "," << vals[i][1]<< ") out of range" << std::endl;
+            errCaught = true;
+        }
+        CHECK(errCaught);
     }
-    CHECK(errCaught);
-
-    errCaught = false;
-    try {
-        s.at(5,1) = 100;
-    } catch (const std::out_of_range& e) {
-        std::cout << "at(5,1) out of range" << std::endl;
-        errCaught = true;
-    }
-    CHECK(errCaught);
-
-    errCaught = false;
-    try {
-        s.at(-1,-1) = 100;
-    } catch (const std::out_of_range& e) {
-        std::cout << "at(-1,-1) out of range" << std::endl;
-        errCaught = true;
-    }
-    CHECK(errCaught);
 }
 
 TEST_CASE("big 5"){
     std::cout << "\ntesting big 5\n" << std::endl;
 
     // copy operator 
+    std::cout << "copy operator" << std::endl;
     a = s;
     a.print();
     s.print();
@@ -105,6 +171,7 @@ TEST_CASE("big 5"){
 
 
     // copy constructor  
+    std::cout << "copy constructor" << std::endl;
     SquareMatrix<int> b = a;
     a.print();
     b.print();
@@ -114,6 +181,7 @@ TEST_CASE("big 5"){
     
 
     // move operator 
+    std::cout << "move operator" << std::endl;
     b = a + s;
     b.print();
 
@@ -122,6 +190,7 @@ TEST_CASE("big 5"){
 
 
     // move constructor 
+    std::cout << "move constructor" << std::endl;
     SquareMatrix<int> d = a + s;
     d.print();
 
@@ -134,6 +203,10 @@ TEST_CASE("big 5"){
 TEST_CASE("+"){
     std::cout << "\ntesting + operator\n" << std::endl;
     
+    std::cout << "(a + s).print()" << std::endl;
+    (a + s).print();
+
+    std::cout << "construct d from a + s" << std::endl;
     SquareMatrix<int> d = a + s;
 
     a.print();
@@ -149,46 +222,69 @@ TEST_CASE("+"){
     bool c = d == a + s ;
     CHECK(c);
 
+    std::cout << "d = d + a" << std::endl;
+    d = d + a;
+    d.print();
 
+    std::cout << "d = d + a + s" << std::endl;
+    d = d + a + s;
+    d.print();
+
+    std::cout << "(d + s).print()" << std::endl;
+    (d + s).print();
+
+    std::cout << "(a + d + s).print()" << std::endl;
+    (a + d + s).print();
+
+    m.print();
     m.resize(8);
-    for (int r = 0; r < 5; r++) {
-        for (int c = 0; c < 5; c++) {
+    for (int r = 0; r < 8; r++) {
+        for (int c = 0; c < 8; c++) {
             m.at(r,c) = r*10 + c;
         }
     }
-
-
+    m.print();
+    a.print();
+    
+    // throw exception if matrices aren't the same size 
     bool errCaught = false;
     try {
         a + m;
     } catch (const std::invalid_argument& e) {
-        std::cout << "invalid matrix given" << std::endl;
+        std::cout << "invalid matrix given\n" << std::endl;
         errCaught = true;
     }
     CHECK(errCaught);
 
-
+    m.print();
+    a.print();
 }
 
 
 TEST_CASE("mentioned"){
     std::cout << "\ntesting stuff mentioned in spec\n" << std::endl;
+
+    std::cout << "default constructor / zero param constructor" << std::endl;
+
     SquareMatrix<int> b;
-    std::cout << "b" << std::endl;
+    std::cout << "b" << "\tsize: " << b.size() << std::endl;; 
     b.print();
     b = a + s;
     std::cout << "b" << std::endl;
     b.print();
+
     SquareMatrix<int> d = b + b;
     std::cout << "d" << std::endl;
     d.print();
+
 
     std::cout << "a = a" << std::endl;
     a = a;
     std::cout << "a" << std::endl;
     a.print();
 
-    std::cout << "a = b = d" << std::endl;
+
+    std::cout << "a = b = c" << std::endl;
     a = b = d;
     std::cout << "a" << std::endl;
     a.print();
@@ -197,12 +293,19 @@ TEST_CASE("mentioned"){
     std::cout << "d" << std::endl;
     d.print();
 
-    CHECK(true);
+    bool c = a == d;
+    CHECK(c);
+    c = a == b;
+    CHECK(c);
+    c = b == d;
+    CHECK(c);
 }
 
 
 
 /*
+    std::cout << "" << std::endl;
+
 TEST_CASE(""){
     CHECK();
 }
