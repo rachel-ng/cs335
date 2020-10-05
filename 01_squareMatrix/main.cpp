47783360ba3e19a0e433f29847bb2362b937a8df
rachel-ng <rng@stuy.edu>
2020-10-04 20:29:37 -0400

    main

--- 01_squareMatrix/main.cpp
+++ 01_squareMatrix/main.cpp
@@ -7,7 +7,6 @@ int main() {
     SquareMatrix<int> b(10);
     std::cout << (s == b ? "match" : "no match") << std::endl;
     s.at(2,1) = 100;
-    s.print(); 
     std::cout << std::endl;
     if (s == b) { 
         std::cout << "match" << std::endl;
@@ -21,6 +20,7 @@ int main() {
     b.print(); 
     std::cout << std::endl;
     std::cout << (s == b ? "match" : "no match") << std::endl;
+    std::cout << (s != b ? "match" : "no match") << std::endl;
     std::cout << std::endl;
     s.resize(5); 
     std::cout << std::endl;
@@ -29,9 +29,21 @@ int main() {
     s.at(2,1) = 12341234;
     s.print(); 
     std::cout << s.at(2,1) << std::endl;
+    try {
+        std::cout << s.at(6,1) << std::endl;
+    }
+    catch (const std::out_of_range& err) {
+        std::cout << "haha bitch at(6,1) is invalid" << std::endl;
+    }
     std::cout << s.at(3,1) << std::endl;
-    std::cout << s.at(5,1) << std::endl;
-    std::cout << s.at(6,1) << std::endl;
+    
+    try {
+        std::cout << s.at(5,1) << std::endl;
+    }
+    catch (const std::out_of_range& err) {
+        std::cout << "haha bitch at(5,1) is invalid" << std::endl;
+    }
+
     s.resize(6); 
     s.print(); 
     return 0;
